95a2204dbb0f99f4c9e6cf588692af6c5b86d458
abhayXdev <abhaypaul058@gmail.com>
2026-06-30 21:12:43 +0530

    fix: correct Badge prop types to match rendered span element

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface BadgeProps extends React.HTMLAttributes<HTMLSpanElement> {
   variant?: "default" | "green" | "orange" | "outline";
 }
 
